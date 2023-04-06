5ee6594c595af38ce293c2339577645a91e8aef0
Bohdan Makarenko <bohdan.makarenko@blackbird.io>
2023-04-06 17:33:40 +0300

    Trailing slash in url bug fixed

--- Apps.Wordpress/CustomWordpressClient.cs
+++ Apps.Wordpress/CustomWordpressClient.cs
@@ -10,7 +10,7 @@ namespace Apps.Wordpress
 {
     public class CustomWordpressClient : WordPressClient
     {
-        public CustomWordpressClient(string url, string login, string appPassword) : base(url)
+        public CustomWordpressClient(string url, string login, string appPassword) : base($"{url.TrimEnd('/')}/")
         {
             this.Auth.UseBasicAuth(login, appPassword);
         }

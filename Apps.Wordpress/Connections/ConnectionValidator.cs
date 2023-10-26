584e69dd6ca5165780583b05840b188ba1c8122b
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-10-26 16:26:59 +0300

    preparations for the artifactory

---
+++ Apps.Wordpress/Connections/ConnectionValidator.cs
@@ -0,0 +1,33 @@
+﻿using Apps.Wordpress.Api;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.Wordpress.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
+        CancellationToken cancellationToken)
+    {
+        var client = new CustomWordpressClient(authenticationCredentialsProviders);
+
+        try
+        {
+            await client.Users.GetAllAsync(false, true);
+            return new ConnectionValidationResponse
+            {
+                IsValid = true,
+                Message = "Success"
+            };
+        }
+        catch (Exception)
+        {
+            return new ConnectionValidationResponse
+            {
+                IsValid = false,
+                Message = "Invalid connection parameters"
+            };
+        }
+    }
+}
